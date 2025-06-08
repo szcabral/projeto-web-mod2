2ec8fed0e449a6fde34a60b327e4675e1e24fa65
Isabela Szolnoky Ferreira Cabral Souza da Conceição <130784620+szcabral@users.noreply.github.com>
2025-06-08 20:35:49 +0000

    fix: corrigindo estrutura de pastas.

---
+++ src/server.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const db = require('./config/database');
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+db.connect()
+  .then(() => {
+    console.log('Conectado ao banco de dados PostgreSQL');
+
+    app.use(express.json());
+
+    const indexRouter = require('./routes/index');
+
+    // Middleware para lidar com erros de rota não encontrada
+    app.use((req, res, next) => {
+      res.status(404).send('Página não encontrada');
+    });
+
+    // Middleware para lidar com erros internos do servidor
+    app.use((err, req, res, next) => {
+      console.error(err.stack);
+      res.status(500).send('Erro no servidor');
+    });
+
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log(`Servidor rodando na porta ${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.error('Erro ao conectar ao banco de dados:', err);
+  });
