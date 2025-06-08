dbf96076ebdc4f24f1e54f22c34c20e16943e93c
Isabela Szolnoky Ferreira Cabral Souza da Conceição <130784620+szcabral@users.noreply.github.com>
2025-06-08 21:13:25 +0000

    fix: corrigindo home em server e mudando seu final com terminação .ejs e adicionando uma pasta public para as imagens ficarem organizadas.

--- src/server.js
+++ src/server.js
@@ -25,6 +25,13 @@ db.connect()
     app.use('/eventos', eventosRoutes);
     app.use('/agendamentos', agendamentosRoutes);
 
+    app.use(express.static(path.join(__dirname, 'public')));
+
+     app.get('/', (req, res) => {
+      res.render('pages/home');
+    });
+
+
     app.use((req, res, next) => {
       res.status(404).send('Página não encontrada');
     });
