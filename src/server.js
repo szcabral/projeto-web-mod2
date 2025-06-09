56c2ef1b633f888ccd1928624b4a20ec4b91dac9
Isabela Szolnoky Ferreira Cabral Souza da Conceição <130784620+szcabral@users.noreply.github.com>
2025-06-09 02:32:16 +0000

    feat: adicionando rota ao server.js

--- src/server.js
+++ src/server.js
@@ -4,18 +4,16 @@ const app = express();
 const db = require('./config/database');
 const path = require('path');
 
-// Configurações do EJS
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Conexão com o banco
 db.connect()
   .then(() => {
     console.log('Conectado ao banco de dados PostgreSQL');
 
     app.use(express.json());
 
-    // Rotas principais da aplicação
     const clientesRoutes = require('./routes/clientesRoutes');
     const funcionariosRoutes = require('./routes/funcionariosRoutes');
     const servicosRoutes = require('./routes/servicosRoutes');
@@ -28,10 +26,10 @@ db.connect()
     app.use('/eventos', eventosRoutes);
     app.use('/agendamentos', agendamentosRoutes);
 
-    // Servir arquivos estáticos da pasta public
+
     app.use(express.static(path.join(__dirname, 'public')));
 
-    // Rotas de páginas
+
     app.get('/', (req, res) => {
       res.render('pages/home');
     });
@@ -48,27 +46,27 @@ db.connect()
       res.render('pages/funcionarioDashboard');
     });
 
-    // Rota para o dashboard do cliente
     app.get('/clienteDashboard', (req, res) => {
       res.render('pages/clienteDashboard'); // clienteDashboard.ejs deve estar em views/pages
     });
 
     app.get('/eventoNovo', (req, res) => {
-  res.render('pages/eventoNovo'); // renderiza views/pages/novoEvento.ejs
+  res.render('pages/eventoNovo');
     });
-    
-    // Tratamento de erro 404
+
+        app.get('/eventoNovoFuncionario', (req, res) => {
+  res.render('pages/eventoNovoFuncionario');
+    });
+
     app.use((req, res, next) => {
       res.status(404).send('Página não encontrada');
     });
 
-    // Tratamento de erro do servidor
     app.use((err, req, res, next) => {
       console.error(err.stack);
       res.status(500).send('Erro no servidor');
     });
 
-    // Inicialização do servidor
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
       console.log(`Servidor rodando na porta ${PORT}`);
