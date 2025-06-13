e0d768215409b2f58186f6e352c329e8ba0940d3
Isabela Szolnoky Ferreira Cabral Souza da Conceição <130784620+szcabral@users.noreply.github.com>
2025-06-13 13:29:17 +0000

    fix: alterando ejs do clienteDashboard.

--- src/server.js
+++ src/server.js
@@ -71,7 +71,7 @@ db.connect()
       res.status(500).send('Erro no servidor');
     });
 
-    const PORT = process.env.PORT || 3000;
+    const PORT = process.env.PORT || 3001;
     app.listen(PORT, () => {
       console.log(`Servidor rodando na porta ${PORT}`);
     });
